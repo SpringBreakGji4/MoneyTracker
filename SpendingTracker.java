44ce2f43e200abfcfd513bb2f85aaf6985f27893
SpringBreak <timm4205@gmail.com>
2023-03-15 23:48:13 -0500

    Add exit button

--- SpendingTracker.java
+++ SpendingTracker.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 import javax.swing.*;
 
 public class SpendingTracker extends JFrame implements ActionListener{
@@ -18,14 +19,14 @@ public class SpendingTracker extends JFrame implements ActionListener{
         	costField = new JTextField(10);
         	totalField = new JTextField(10);
         	totalField.setEditable(false);
-        	itemList = new JTextArea(10, 20);
+        	itemList = new JTextArea(5, 20);
         	JScrollPane scrollPane = new JScrollPane(itemList);
         	JButton addButton = new JButton("Add");
         	addButton.addActionListener(this);
 		
 		setLayout(new GridBagLayout());
         	GridBagConstraints c = new GridBagConstraints();
-        	c.insets = new Insets(5, 5, 5, 5);
+        	c.insets = new Insets(5, 5, 5, 5);  // set the external padding
         	c.gridx = 0;
         	c.gridy = 0;
         	add(new JLabel("Item:"), c);
@@ -51,7 +52,22 @@ public class SpendingTracker extends JFrame implements ActionListener{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         	setSize(400, 300);
         	setLocationRelativeTo(null);
-        	setVisible(true);
+        	
+		JButton exitButton = new JButton("Exit");
+		exitButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				System.exit(0);
+			}
+		});
+		add(exitButton, BorderLayout.SOUTH);
+		pack();
+			
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = screenSize.width - getWidth();
+		int y = screenSize.height - getHeight();
+		setLocation(x, y);
+		
+		setVisible(true);
 
 	}
 	public void actionPerformed(ActionEvent e){
@@ -62,5 +78,12 @@ public class SpendingTracker extends JFrame implements ActionListener{
        	 	totalField.setText("$" + total);
         	itemField.setText("");
         	costField.setText("");
+		
+		try (FileWriter fw = new FileWriter("spending.txt", true); BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
+            		out.println(item + "," + cost);
+        	}	 
+		catch (IOException ex){
+            		System.err.println("Error writing to file: " + ex.getMessage());
+        	}
 	}
 }
